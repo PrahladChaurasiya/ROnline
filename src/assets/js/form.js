e73f765db634da6cb5168f7b8862e9e86e9dd65a
Prahlad Chaurasiya <prahladchaurasiya@yahoo.in>
2019-03-29 17:37:48 +0530

    Add files via upload

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -1,81 +1,128 @@
-/* 
-Orginal Page: http://thecodeplayer.com/walkthrough/jquery-multi-step-form-with-progress-bar 
-
-*/
-//jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
-
-$(".next").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
-	
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-	//show the next fieldset
-	next_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
-			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
-			left = (now * 50)+"%";
-			//3. increase opacity of next_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'transform': 'scale('+scale+')'});
-			next_fs.css({'left': left, 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
-});
-
-$(".previous").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	previous_fs = $(this).parent().prev();
-	
-	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
-	//show the previous fieldset
-	previous_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
-			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
-});
-
-
-
+// Profile Education Work Skills Achievements
+		var profile = document.getElementById("profile");
+		var profileLink = document.getElementById("profile-link");
+		var education = document.getElementById("education");
+		var educationLink = document.getElementById("education-link");
+		var work = document.getElementById("work");
+		var workLink = document.getElementById("work-link");
+		var skills = document.getElementById("skills");
+		var skillsLink = document.getElementById("skills-link");
+		var achievements = document.getElementById("achievements");
+		var achievementsLink = document.getElementById("achievements-link");
+		var projects = document.getElementById("projects");
+		var projectsLink = document.getElementById("projects-link");
+		
+		function openProfile() {
+			profile.style.display = "block";
+			education.style.display = "none";
+			work.style.display = "none";
+			skills.style.display = "none";
+			achievements.style.display = "none";
+			projects.style.display = "none";
+			
+			// add red background to profile button and remove from other buttons
+			if(!profileLink.classList.contains("selected")) {
+				profileLink.classList.add("selected");
+					educationLink.classList.remove("selected");
+					workLink.classList.remove("selected");
+					skillsLink.classList.remove("selected");
+					achievementsLink.classList.remove("selected");
+					projectsLink.classList.remove("selected");
+			}
+		}
+		
+		function openEducation() {
+			education.style.display = "block";
+			profile.style.display = "none";
+			work.style.display = "none";
+			skills.style.display = "none";
+			achievements.style.display = "none";
+			projects.style.display = "none";
+			
+			// add red background to education button and remove from other buttons
+			if(!educationLink.classList.contains("selected")) {
+				educationLink.classList.add("selected");
+					profileLink.classList.remove("selected");
+					workLink.classList.remove("selected");
+					skillsLink.classList.remove("selected");
+					achievementsLink.classList.remove("selected");
+					projectsLink.classList.remove("selected");
+			}
+		}
+		
+		function openWork() {
+			work.style.display = "block";
+			profile.style.display = "none";
+			education.style.display = "none";
+			skills.style.display = "none";
+			achievements.style.display = "none";
+			projects.style.display = "none";
+			
+			// add red background to work button and remove from other buttons
+			if(!workLink.classList.contains("selected")) {
+				workLink.classList.add("selected");
+					profileLink.classList.remove("selected");
+					educationLink.classList.remove("selected");
+					skillsLink.classList.remove("selected");
+					achievementsLink.classList.remove("selected");
+					projects.classList.remove("selected");
+			}
+		}
+		
+		function openSkills() {
+			skills.style.display = "block";
+			work.style.display = "none";
+			education.style.display = "none";
+			profile.style.display = "none";
+			achievements.style.display = "none";
+			projects.style.display = "none";
+			
+			// add red background to skills button and remove from other buttons
+			if(!skillsLink.classList.contains("selected")) {
+				skillsLink.classList.add("selected");
+					profileLink.classList.remove("selected");
+					educationLink.classList.remove("selected");
+					workLink.classList.remove("selected");
+					achievementsLink.classList.remove("selected");
+					projectsLink.classList.remove("selected");
+			}
+		}
+		
+		function openAchievements() {
+			achievements.style.display = "block";
+			work.style.display = "none";
+			education.style.display = "none";
+			profile.style.display = "none";
+			skills.style.display = "none";
+			projects.style.display = "none";
+			
+			// add red background to achievements button and remove from other buttons
+			if(!achievementsLink.classList.contains("selected")) {
+				achievementsLink.classList.add("selected");
+					profileLink.classList.remove("selected");
+					educationLink.classList.remove("selected");
+					workLink.classList.remove("selected");
+					skillsLink.classList.remove("selected");
+					projectsLink.classList.remove("selected");
+			}
+		}
+		
+		function openProjects() {
+			projects.style.display = "block";
+			achievements.style.display = "none";
+			work.style.display = "none";
+			education.style.display = "none";
+			profile.style.display = "none";
+			skills.style.display = "none";
+			
+			// add red background to projects button and remove from other buttons
+			if(!projectsLink.classList.contains("selected")) {
+				projectsLink.classList.add("selected");
+					profileLink.classList.remove("selected");
+					educationLink.classList.remove("selected");
+					workLink.classList.remove("selected");
+					skillsLink.classList.remove("selected");
+					achievementsLink.classList.remove("selected");
+			}
+		}
+		
